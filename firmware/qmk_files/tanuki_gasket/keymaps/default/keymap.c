8c769f6e0beaa695d5d0db5b2639a36e17d4a00d
Gustavo Chen <gustavo.sao@gmail.com>
2025-05-22 21:38:52 -0300

    Add maplestory layer

--- firmware/qmk_files/tanuki_gasket/keymaps/default/keymap.c
+++ firmware/qmk_files/tanuki_gasket/keymaps/default/keymap.c
@@ -13,6 +13,7 @@ user_config_t user_config;
 #define _UL 2
 #define _GL 3
 #define _GLV 4
+#define _MSL 5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -29,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7,
         KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_1, KC_2, KC_3, KC_4,
-        KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, TG(_GLV), KC_TRNS, KC_PSCR, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, TG(_GLV), KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(_MSL), KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, QK_BOOT, KC_TRNS, KC_TRNS, KC_LGUI, KC_TRNS, KC_TRNS),
 
     [_UL] = LAYOUT(
@@ -54,9 +55,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
                KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,
                KC_P, KC_BSPC, KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J,
-               KC_K, KC_L, KC_SCLN, TG(_GLV), KC_LSFT, KC_Z, KC_X, KC_C, KC_V,
-               KC_B, KC_N, KC_M, KC_QUOT, KC_SLSH, KC_ENT, KC_LCTL, KC_LALT,
-               KC_COMMA, KC_SPC, KC_SPC, KC_DOT, KC_LGUI)};
+               KC_K, KC_L, KC_SCLN, KC_TRNS, KC_LSFT, KC_Z, KC_X, KC_C, KC_V,
+               KC_B, KC_N, KC_M, KC_QUOT, KC_SLSH, KC_ENT, KC_COMMA, KC_LALT,
+               KC_LCTL, KC_SPC, KC_SPC, KC_DOT, KC_LGUI),
+
+    [_MSL] =
+        LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
+               KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_UP,
+               KC_P, KC_BSPC, KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J,
+               KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_LSFT, KC_Z, KC_X, KC_C,
+               KC_V, KC_B, KC_N, KC_M, KC_QUOT, KC_SLSH, KC_ENT, KC_LCTL,
+               KC_LALT, KC_COMMA, KC_LALT, KC_SPC, KC_DOT, KC_LGUI)};
 
 void keyboard_post_init_user(void) { user_config.raw = eeconfig_read_user(); }
 
